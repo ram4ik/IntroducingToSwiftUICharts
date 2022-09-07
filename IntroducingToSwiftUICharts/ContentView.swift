83f71b2e8b39062257336e3af6b040e6c4899b20
Ramill <ramill_ibragimov@icloud.com>
2022-09-07 14:50:21 +0300

    init

--- IntroducingToSwiftUICharts/ContentView.swift
+++ IntroducingToSwiftUICharts/ContentView.swift
@@ -6,16 +6,37 @@
 //
 
 import SwiftUI
+import Charts
+
+enum AccountType: String {
+    case checking = "Checking"
+    case savings = "Savings"
+    case brokerage = "Brokerage"
+    case retirement = "Retirement"
+}
+
+struct Account: Identifiable {
+    let id = UUID()
+    let accountType: AccountType
+    let balance: Double
+}
 
 struct ContentView: View {
+    
+    let accounts = [
+        Account(accountType: .checking, balance: 4500),
+        Account(accountType: .savings, balance: 8000),
+        Account(accountType: .retirement, balance: 14500),
+        Account(accountType: .brokerage, balance: 6500),
+    ]
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
-        }
-        .padding()
+        Chart {
+            ForEach(accounts) { account in
+                BarMark(x: .value("Account Type", account.accountType.rawValue), y: .value("Balance", account.balance))
+                    .foregroundStyle(by: .value("Account Type", account.accountType.rawValue))
+            }
+        }.padding()
     }
 }
 

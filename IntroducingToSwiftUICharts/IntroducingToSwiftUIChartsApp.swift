c1ff5243d0b09391748716bb992580ebe7e88df1
Ramill <ramill_ibragimov@icloud.com>
2022-09-07 14:42:09 +0300

    Initial Commit

---
+++ IntroducingToSwiftUICharts/IntroducingToSwiftUIChartsApp.swift
@@ -0,0 +1,17 @@
+//
+//  IntroducingToSwiftUIChartsApp.swift
+//  IntroducingToSwiftUICharts
+//
+//  Created by Ramill Ibragimov on 07.09.2022.
+//
+
+import SwiftUI
+
+@main
+struct IntroducingToSwiftUIChartsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
